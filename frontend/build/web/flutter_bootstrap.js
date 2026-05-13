c5495ba080410552f5eb6c876d9b30bcbcc6c0f9
KooKooTrain <divya7bhosale@gmail.com>
2026-05-13 13:05:22 +0400

    deploy

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1339870671" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2989731337" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
