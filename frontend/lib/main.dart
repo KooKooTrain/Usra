bbe93b9629d449e0f22ce160742d677b166cc1a4
KooKooTrain <divya7bhosale@gmail.com>
2026-05-13 13:04:36 +0400

    fix env + ai api

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,23 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
 import 'screens/signup.dart';
 import 'screens/home.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const Usra());
 }
 
 class Usra extends StatelessWidget {
   const Usra({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Usra App',
